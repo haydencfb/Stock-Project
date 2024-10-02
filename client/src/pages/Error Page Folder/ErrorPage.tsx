9a62ecb0d88ccd5f4e779e9226c54567edf9052d
haydencfb <haydenfitzbrintle@gmail.com>
2024-10-01 20:49:08 -0700

    error page styled

--- client/src/pages/Error Page Folder/ErrorPage.tsx
+++ client/src/pages/Error Page Folder/ErrorPage.tsx
@@ -1,6 +1,19 @@
+import classes from '../Error Page Folder/error.module.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 const ErrorPage = () => {
     return (
-        <h1>This is an error</h1>
+        <>
+            <Box>
+                <Typography variant='h1' display='flex' justifyContent='center' className={`${classes.error}`}>
+                    This is an Error
+                </Typography>
+                <Typography variant='h1' display='flex' justifyContent='center' className={`${classes.error}`}>
+                    ¯\_(ツ)_/¯
+                </Typography>
+            </Box>
+        </>
     )
 }
 
