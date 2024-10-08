f2a9c0dfae16607d4742305613f4da3f81d69aa7
John Marquez <johnmarquez249@gmail.com>
2024-10-08 16:28:04 -0600

    Implemented JWT using auth-routes/ auth.js in server side

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,27 +1,24 @@
-import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-export const authenticateToken = (
-    req,
-    res,
-    next
-    ) => {
-    const authHeader = req.headers.authorization;
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
 
-        const secretKey = process.env.JWT_SECRET_KEY || '';
+    const secretKey = process.env.JWT_SECRET_KEY || '';
 
-        jwt.verify(token, secretKey, (err, user) => {
-        if (err) {
-            return res.sendStatus(403); // Forbidden
-        }
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403); // Forbidden
+      }
 
-        req.user = user;
-        return next();
-        });
-    } else {
-        res.sendStatus(401); // Unauthorized
-    }
+      req.user = user; // No need for explicit type casting like in TypeScript
+      return next();
+    });
+  } else {
+    res.sendStatus(401); // Unauthorized
+  }
 };
+
+export default authenticateToken;
